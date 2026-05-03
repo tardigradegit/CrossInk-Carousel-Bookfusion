a4e76a40bcda869331b8d467aab910c9d8cf1d34
emilylawver-hub <emilylawver@gmail.com>
2026-05-03 18:57:29 -0400

    Add BookFusion token store declarations to JsonSettingsIO.h

--- src/JsonSettingsIO.h
+++ src/JsonSettingsIO.h
@@ -1,36 +1,51 @@
 #pragma once
 
+
 class CrossPointSettings;
 class CrossPointState;
 class WifiCredentialStore;
 class KOReaderCredentialStore;
+class BookFusionTokenStore;
 class RecentBooksStore;
 class OpdsServerStore;
 
+
 namespace JsonSettingsIO {
+
 
 // CrossPointSettings
 bool saveSettings(const CrossPointSettings& s, const char* path);
 bool loadSettings(CrossPointSettings& s, const char* json, bool* needsResave = nullptr);
 
+
 // CrossPointState
 bool saveState(const CrossPointState& s, const char* path);
 bool loadState(CrossPointState& s, const char* json);
+
 
 // WifiCredentialStore
 bool saveWifi(const WifiCredentialStore& store, const char* path);
 bool loadWifi(WifiCredentialStore& store, const char* json, bool* needsResave = nullptr);
 
+
 // KOReaderCredentialStore
 bool saveKOReader(const KOReaderCredentialStore& store, const char* path);
 bool loadKOReader(KOReaderCredentialStore& store, const char* json, bool* needsResave = nullptr);
+// BookFusionTokenStore
+bool saveBookFusion(const BookFusionTokenStore& store, const char* path);
+bool loadBookFusion(BookFusionTokenStore& store, const char* json);
+
+
 
 // RecentBooksStore
 bool saveRecentBooks(const RecentBooksStore& store, const char* path);
 bool loadRecentBooks(RecentBooksStore& store, const char* json);
 
+
 // OpdsServerStore
 bool saveOpds(const OpdsServerStore& store, const char* path);
 bool loadOpds(OpdsServerStore& store, const char* json, bool* needsResave = nullptr);
 
+
 }  // namespace JsonSettingsIO
+
